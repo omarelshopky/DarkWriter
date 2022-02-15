ea50e2644653280ec9e68b6fb0f44f8bd9a01013
Omar Hesham <oommaarr.220022@gmail.com>
2022-02-15 04:40:27 +0200

    Pass the Blocking period and type to the Writing Window

--- OptionWindow.py
+++ OptionWindow.py
@@ -30,14 +30,12 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
         # Browse button event
         self.odBrowseBtn.clicked.connect(lambda:self.browseFile())
 
-        # Enable Start writing without blocking
-        self.ndFreeRadioBtn.toggled.connect(lambda:self.enableFreeStart())
-        self.odFreeRadioBtn.toggled.connect(lambda:self.enableFreeStart())
-        self.odFilePathTxt.textChanged.connect(lambda:self.enableFreeStart())
-
-        # 
-        self.ndBlockInput.textChanged.connect(lambda:self.enableBlockedStart())
-
+        # Enable Start Writing Events
+        self.ndFreeRadioBtn.toggled.connect(lambda:self.enableStart())
+        self.odFreeRadioBtn.toggled.connect(lambda:self.enableStart())
+        self.odFilePathTxt.textChanged.connect(lambda:self.enableStart())
+        self.ndBlockInput.textChanged.connect(lambda:self.enableStart())
+        self.odBlockInput.textChanged.connect(lambda:self.enableStart())
 
 
     # Make blocking input box accept only integers
@@ -50,10 +48,37 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
     def openWrintingWindow(self):
         # Current Index = 0 at New Draft Tab
         filePath = self.pathSolver.getNewFilePath() if self.tabWidget.currentIndex() == 0 else self.odFilePathTxt.text()
+        self.setBlocking()
         self.mainWidget.widget(1).setFilePath(filePath)
-        self.mainWidget.widget(1).setBlocking(self.blockingAmount, self.blockingInTime)
         self.mainWidget.setCurrentIndex(1)
 
+
+    # Set Blocking period and Type
+    def setBlocking(self):
+        if self.tabWidget.currentIndex() == 0:  # New Draft Tab
+            if self.ndFreeRadioBtn.isChecked():
+                amount = 0
+                isTime = True
+            elif self.ndMntRadioBtn.isChecked():
+                amount = self.ndBlockInput.text()
+                isTime = True
+            elif self.ndWordRadioBtn.isChecked():
+                amount = self.ndBlockInput.text()
+                isTime = False
+            
+        else:           # Open Draft Tab
+            if self.odFreeRadioBtn.isChecked():
+                amount = 0
+                isTime = True
+            elif self.odMntRadioBtn.isChecked():
+                amount = self.odBlockInput.text()
+                isTime = True
+            elif self.odWordRadioBtn.isChecked():
+                amount = self.odBlockInput.text()
+                isTime = False
+
+        self.mainWidget.widget(1).setBlocking(amount, isTime)
+        
 
     # Close the app
     def closeApp(self):
@@ -67,32 +92,20 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
         self.odFilePathTxt.setText(fname[0])
 
 
-    # Enable start writing with blocking time = 0 min
-    def enableFreeStart(self):
-        
-        if self.tabWidget.currentIndex() == 0:  # New Draft Tab
-            self.ndStartBtn.setEnabled(self.ndFreeRadioBtn.isChecked())
-
-        else:           # Open Draft Tab
-            if self.odFreeRadioBtn.isChecked() and self.odFilePathTxt.text() != '':
-                self.odStartBtn.setEnabled(True)
-            else:
-                self.odStartBtn.setEnabled(False)
-
-        self.blockingInTime = True
-        self.blockingAmount = 0
-
-
-    def enableBlockedStart(self):
+    # Enable Start Writing Button
+    def enableStart(self):
 
         if self.tabWidget.currentIndex() == 0:  # New Draft Tab
-            if self.ndBlockInput.text() != '':
+            if self.ndBlockInput.text() != '' or self.ndFreeRadioBtn.isChecked():
                 self.ndStartBtn.setEnabled(True)
             else:
                 self.ndStartBtn.setEnabled(False)
 
         else:           # Open Draft Tab
-            pass
+            if (self.odBlockInput.text() != '' or self.odFreeRadioBtn.isChecked()) and self.odFilePathTxt.text() != '':
+                self.odStartBtn.setEnabled(True)
+            else:
+                self.odStartBtn.setEnabled(False)
 
 
 
