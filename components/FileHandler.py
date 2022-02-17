86cafc17ff061e61d934a230a59a580c32133566
Omar Hesham <oommaarr.220022@gmail.com>
2022-02-17 04:41:10 +0200

    Auto saving each 10s

--- components/FileHandler.py
+++ components/FileHandler.py
@@ -1,18 +1,34 @@
 from components.SaveStatusMessage import SaveStatusMessage
 from PyQt5.QtWidgets import * 
+from PyQt5.QtCore import * 
+
 
 class FileHandler:
     def __init__(self, parent):
         self.parent = parent
         self.saveStatusMessage = SaveStatusMessage(parent)
 
+        # Timer to Save the file each 10s
+        self.autosaveTimer = QTimer()
+        self.autosaveTimer.timeout.connect(lambda: self.autosave())
+        self.autosaveTime.start(10000)
+
+    
+    # Save the file each 
+    def autosave(self):
+        self.autosaveTimer.stop()
+        self.saveToFile()
+        self.autosaveTime.start(10000)
+        
 
     def setFilePath(self, path):
         self.filePath = path
 
 
-    def saveToFile(self, contentLines, close = False):
+    def saveToFile(self, close = False):
         try:
+            self.parent.setContent()
+            
             if close:
                 # Open browse to enable user to choose a location
                 fname = QFileDialog.getSaveFileName(self.parent, 'Save File', self.filePath, 'Text Files (*.txt)')[0]
@@ -24,7 +40,7 @@ class FileHandler:
         
             with open(self.filePath, 'w') as file:
                 text = ''
-                for paragraph in contentLines:
+                for paragraph in self.parent.contentLines:
                     text += '\n'.join(paragraph)
                     text += '\n\n'
 
