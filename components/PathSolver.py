580a00faf98a7dc4ee212738ad2c8355c7e0ff62
Omar Hesham <oommaarr.220022@gmail.com>
2022-02-19 23:40:47 +0200

    Use additional way to get my documents path

--- components/PathSolver.py
+++ components/PathSolver.py
@@ -1,14 +1,31 @@
 import os
 from datetime import datetime
+import ctypes.wintypes
 
 
 class PathSolver():
 
     # Gets the path of the new created file in default dir
     def getNewFilePath(self):
-        defaultPath = os.path.expanduser('~/Documents/DarkTurkeyWriter/')
+        try:
+            defaultPath = os.path.expanduser('~/My Documents/KEYBOARDCOWBOY/')
+            filePath = os.path.join(defaultPath, self.getCurrentDateTime() + '.txt')
+
+            with open(filePath, 'w') as f:
+                f.write('\n')
+                
+        except:
+            CSIDL_PERSONAL = 5       # My Documents
+            SHGFP_TYPE_CURRENT = 0   # Get current, not default value
+
+            buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+            ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+
+            filePath = os.path.join(buf.value, self.getCurrentDateTime() + '.txt')
+
+            with open(filePath, 'w') as f:
+                f.write('\n')
         
-        filePath = os.path.join(defaultPath, self.getCurrentDateTime() + '.txt')
         if not os.path.exists(defaultPath):
             os.makedirs(defaultPath)
             
