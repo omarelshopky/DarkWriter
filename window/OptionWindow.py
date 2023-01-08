84a0ff5976f752c090927af8ccd8a2ecf73a509f
Omar Hesham <omar.elshopky202@gmail.com>
2023-01-08 19:15:39 +0200

    MULTSESS | Add ability to divide the goal into smaller sessions

--- window/OptionWindow.py
+++ window/OptionWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QStackedWidget
+from PyQt5.QtWidgets import QMainWindow, QStackedWidget, QFileDialog
 from PyQt5.QtGui import QIntValidator
 from cenum.AppWindow import AppWindow
 from cenum.OptionTab import OptionTab
@@ -14,23 +14,8 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
         Main stacked widget that contains all the app windows
     pathResolver: PathResolver
         The path resolver object that handle creating new files
-
-    Methods
-    -------
-    _setupEvents()
-        Setup all events in the UI components
-    _enableInputValidation()
-        Make blocking input box accept only integers
-    _openWritingWindow(isNewDraft=True)
-        Open the writing window
-    _setBlocking()
-        Set Blocking period and Type
-    _closeApp()
-        Close the app
-    _browseFile()
-        Open browse files dialog
-    _enableStart()
-        Enable Start Writing Button
+    components: dict
+        Dictionary contains tabs related components
     """
     def __init__(self, mainStack: QStackedWidget):
         super().__init__()
@@ -38,11 +23,11 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
         self.pathResolver = PathResolver()
         self.setupUi(self)
         self._setupEvents()
+        self._enableInputValidation()
+        self.components = self._prepareTabComponents()
 
     def _setupEvents(self) -> None:
         """Setup all events in the UI components"""
-        self._enableInputValidation()
-
         # Open writing window
         self.ndStartBtn.clicked.connect(lambda: self._openWritingWindow())
         self.odStartBtn.clicked.connect(lambda: self._openWritingWindow(False))
@@ -55,16 +40,61 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
         self.odBrowseBtn.clicked.connect(lambda: self._browseFile())
 
         # Enable Start Writing Events
+        self.odFilePathTxt.textChanged.connect(lambda: self._enableStart())
         self.ndFreeRadioBtn.toggled.connect(lambda: self._enableStart())
         self.odFreeRadioBtn.toggled.connect(lambda: self._enableStart())
-        self.odFilePathTxt.textChanged.connect(lambda: self._enableStart())
-        self.ndBlockInput.textChanged.connect(lambda: self._enableStart())
-        self.odBlockInput.textChanged.connect(lambda: self._enableStart())
+        self.ndBlockInput.textChanged.connect(lambda: self._handleChangeBlockingAttribute())
+        self.odBlockInput.textChanged.connect(lambda: self._handleChangeBlockingAttribute())
+        self.ndSessionInput.textChanged.connect(lambda: self._handleChangeBlockingAttribute())
+        self.odSessionInput.textChanged.connect(lambda: self._handleChangeBlockingAttribute())
 
     def _enableInputValidation(self) -> None:
         """Make blocking input box accept only integers"""
         self.ndBlockInput.setValidator(QIntValidator())
         self.odBlockInput.setValidator(QIntValidator())
+        self.ndSessionInput.setValidator(QIntValidator())
+        self.odSessionInput.setValidator(QIntValidator())
+
+    def _prepareTabComponents(self) -> dict:
+        """Prepare dictionary contains the similar components in each tab
+
+        Returns:
+        ----------
+        dict:
+            contains the components of each tab
+        """
+        return {
+            OptionTab.NEW_DRAFT.name: {
+                "BlockInput": self.ndBlockInput,
+                "SessionInput": self.ndSessionInput,
+                "TotalBlockTxt": self.ndTotalBlockTxt,
+                "FreeRadioBtn": self.ndFreeRadioBtn,
+                "TimeRadioBtn": self.ndMntRadioBtn,
+                "WordRadioBtn": self.ndWordRadioBtn,
+                "StartBtn": self.ndStartBtn,
+                "QuitBtn": self.ndQuitBtn
+            },
+            OptionTab.OPEN_DRAFT.name: {
+                "BlockInput": self.odBlockInput,
+                "SessionInput": self.odSessionInput,
+                "TotalBlockTxt": self.odTotalBlockTxt,
+                "FreeRadioBtn": self.odFreeRadioBtn,
+                "TimeRadioBtn": self.odMntRadioBtn,
+                "WordRadioBtn": self.odWordRadioBtn,
+                "StartBtn": self.odStartBtn,
+                "QuitBtn": self.odQuitBtn
+            }
+        }
+
+    def _getCurrentTabComponents(self) -> dict:
+        """Get the components of the current tab
+
+        Returns:
+        ----------
+        dict:
+            Dictionary contains all the component of the current tab
+        """
+        return self.components[OptionTab(self.tabWidget.currentIndex()).name]
 
     def _openWritingWindow(self, isNewDraft: bool = True) -> None:
         """Open the writing window
@@ -74,65 +104,62 @@ class OptionWindow(QMainWindow, Ui_optionWindow):
         isNewDraft: bool
             Is new draft or open existing one
         """
-        writingWindow = self.mainStack.widget(AppWindow.WRITING_WINDOW.value)
         filePath = self.pathResolver.getNewFilePath() if self.tabWidget.currentIndex() == OptionTab.NEW_DRAFT.value else self.odFilePathTxt.text()
-        self._setBlocking()
-        writingWindow.setFilePath(filePath)
+        blockingAttributes = self._getBlockingAttributes()
+        self.mainStack.widget(AppWindow.WRITING_WINDOW.value).startBlockingSessions(filePath, isNewDraft, blockingAttributes)
 
-        self.mainStack.setCurrentIndex(AppWindow.WRITING_WINDOW.value)
-        writingWindow.calculateLineHeight()
+    def _getBlockingAttributes(self) -> dict:
+        """Get blocking attributes from input fields
 
-        if isNewDraft == False:
-            writingWindow.fileHandler.loadFromFile()
-            writingWindow.currentParLbl.setText(str(len(writingWindow.contentLines) - 1))
-            writingWindow.updateContent()
-            writingWindow.setCursor(writingWindow.textEdit.document().characterCount() - 1)
-            writingWindow.saving = True
+        Returns:
+        ----------
+        dict:
+            the blocking attributes like amount and is time or words count
+        """
+        components = self._getCurrentTabComponents()
+        amount = components["BlockInput"].text()
+        isTime = not components["WordRadioBtn"].isChecked()
+        numOfSessions = [components["SessionInput"].text(), 1][components["SessionInput"].text() == ""]
 
-    def _setBlocking(self) -> None:
-        """Set Blocking period and Type"""
-        if self.tabWidget.currentIndex() == OptionTab.NEW_DRAFT.value:
-            if self.ndFreeRadioBtn.isChecked():
-                amount = 0
-                isTime = True
-            elif self.ndMntRadioBtn.isChecked():
-                amount = self.ndBlockInput.text()
-                isTime = True
-            elif self.ndWordRadioBtn.isChecked():
-                amount = self.ndBlockInput.text()
-                isTime = False
-        else:# Open Draft Tab
-            if self.odFreeRadioBtn.isChecked():
-                amount = 0
-                isTime = True
-            elif self.odMntRadioBtn.isChecked():
-                amount = self.odBlockInput.text()
-                isTime = True
-            elif self.odWordRadioBtn.isChecked():
-                amount = self.odBlockInput.text()
-                isTime = False
+        if components["FreeRadioBtn"].isChecked():
+            amount = 0
+            numOfSessions = 1
 
-        self.mainStack.widget(AppWindow.WRITING_WINDOW.value).setBlocking(amount, isTime)
+        return {"blockingAmount": amount, "isTimeBlocking": isTime, "numOfSessions": numOfSessions}
 
     def _closeApp(self) -> None:
         """Close the app"""
-        self.close()
         self.mainStack.close()
 
     def _browseFile(self) -> None:
         """Open browse files dialog"""
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '.', 'Text Files (*.txt)')
+        fname = QFileDialog.getOpenFileName(self, "Open file", ".", "Text Files (*.txt)")
         self.odFilePathTxt.setText(fname[0])
 
     def _enableStart(self) -> None:
         """Enable Start Writing Button"""
-        if self.tabWidget.currentIndex() == OptionTab.NEW_DRAFT.value:
-            if self.ndBlockInput.text() != '' or self.ndFreeRadioBtn.isChecked():
-                self.ndStartBtn.setEnabled(True)
-            else:
-                self.ndStartBtn.setEnabled(False)
-        else:# Open Draft Tab
-            if (self.odBlockInput.text() != '' or self.odFreeRadioBtn.isChecked()) and self.odFilePathTxt.text() != '':
-                self.odStartBtn.setEnabled(True)
-            else:
-                self.odStartBtn.setEnabled(False)
+        components = self._getCurrentTabComponents()
+        isAbleToStart = components["BlockInput"].text() != "" or components["FreeRadioBtn"].isChecked()
+
+        if self.tabWidget.currentIndex() == OptionTab.OPEN_DRAFT.value:
+            isAbleToStart = isAbleToStart and self.odFilePathTxt.text() != ""
+
+        components["StartBtn"].setEnabled(isAbleToStart)
+
+    def _calculateTotalBlockGoal(self) -> None:
+        """Calculate total block goal"""
+        components = self._getCurrentTabComponents()
+        blockAmount = components["BlockInput"].text()
+        NumOfSession = components["SessionInput"].text()
+
+        if blockAmount and NumOfSession:
+            components["TotalBlockTxt"].setText(str(int(blockAmount) * int(NumOfSession)))
+        elif blockAmount:
+            components["TotalBlockTxt"].setText(blockAmount)
+        else:
+            components["TotalBlockTxt"].setText("0")
+
+    def _handleChangeBlockingAttribute(self) -> None:
+        """Handle changing text for any of the blocking attributes"""
+        self._calculateTotalBlockGoal()
+        self._enableStart()
